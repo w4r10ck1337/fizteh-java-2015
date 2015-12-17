604880ea2ef0a7bdd1468d14d335627e2e5221b8
Zuev Stepan <zuev.step4n@gmail.com>
2015-12-18 00:33:36 +0300

    checkstyle

--- w4r10ck1337/src/main/java/ru/fizteh/fivt/students/w4r10ck1337/miniorm/annotations/Table.java
+++ w4r10ck1337/src/main/java/ru/fizteh/fivt/students/w4r10ck1337/miniorm/annotations/Table.java
@@ -3,7 +3,7 @@ package ru.fizteh.fivt.students.w4r10ck1337.miniorm.annotations;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-@Retention(value= RetentionPolicy.RUNTIME)
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface Table {
     String name() default "";
 }
